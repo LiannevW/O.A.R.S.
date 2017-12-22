f1e8be997429d803292b7b1301175b829e6fca40
MokumJ <jesse.vanderheijden@gmail.com>
2017-12-22 13:42:12 +0100

    preparing for softdemo

--- src/components/SearchRower.js
+++ src/components/SearchRower.js
@@ -3,6 +3,13 @@ import DropDownMenu from 'material-ui/DropDownMenu';
 import MenuItem from 'material-ui/MenuItem';
 import { connect } from 'react-redux'
 
+const styles = {
+  customWidth: {
+    width: 200,
+  },
+  position: 'fixed',
+}
+
  class SearchRower extends React.Component {
 
    constructor(props) {
@@ -20,26 +27,30 @@ import { connect } from 'react-redux'
    handleChange3 = (event, index, value) => this.setState({value3: value});
    handleChange4 = (event, index, value) => this.setState({value4: value});
 
-   renderRower(rower, index ) {
-     return (
-       <MenuItem key={index} value={index} primaryText={`${rower.firstname} ${rower.lastname}`}/>
-     )
-   }
+   // renderRower(rower, index ) {
+   //   return (
+   // <MenuItem key={index} value={index} primaryText={`${rower.firstname} ${rower.lastname}`}/>
+   //   )
+   // }
   render() {
     const {rowers} = this.props
     return (
       <div>
-      <DropDownMenu value={this.state.value1} onChange={this.handleChange1}>
-        {rowers.map(this.renderRower)}
+      <DropDownMenu value={this.state.value1} onChange={this.handleChange1} style={styles.customWidth} openOnFocus = {true} anchorOrigin>
+        {rowers.map((rower) => (
+        <MenuItem key={rowers} value={rowers} primaryText={`${rower.firstname} ${rower.lastname}`}/> ))}
       </DropDownMenu>
-      <DropDownMenu maxHeight={300} value={this.state.value2} onChange={this.handleChange2}>
-        {rowers.map(this.renderRower)}
+      <DropDownMenu maxHeight={300} value={this.state.value2} onChange={this.handleChange2} style={styles.customWidth}>
+        {rowers.map((rower) => (
+          <MenuItem key={rower} value={rower} primaryText={`${rower.firstname} ${rower.lastname}`}/> ))}
       </DropDownMenu>
-      <DropDownMenu maxHeight={300} value={this.state.value3} onChange={this.handleChange3}>
-        {rowers.map(this.renderRower)}
+      <DropDownMenu maxHeight={300} value={this.state.value3} onChange={this.handleChange3} style={styles.customWidth}>
+        {rowers.map((rower) => (
+          <MenuItem key={rowers} value={rowers} primaryText={`${rower.firstname} ${rower.lastname}`}/> ))}
       </DropDownMenu>
-      <DropDownMenu maxHeight={300} value={this.state.value4} onChange={this.handleChange4}>
-        {rowers.map(this.renderRower)}
+      <DropDownMenu maxHeight={300} value={this.state.value4} onChange={this.handleChange4} style={styles.customWidth}>
+        {rowers.map((rower) => (
+          <MenuItem key={rowers} value={rowers} primaryText={`${rower.firstname} ${rower.lastname}`}/> ))}
       </DropDownMenu>
       </div>
     );
