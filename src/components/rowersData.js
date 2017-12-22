a5973a9e22264a06c1abc5b5f9bf671cb562bf7d
MokumJ <jesse.vanderheijden@gmail.com>
2017-12-22 09:55:28 +0100

    search implement

---
+++ src/components/rowersData.js
@@ -0,0 +1,7 @@
+module.exports = [
+{ firstname: 'Jed', lastname: 'Watson' },
+{ firstname: 'Dave', lastname: 'Backend' },
+{ firstname: 'Lianne', lastname: 'Van Westerink' },
+{ firstname: 'Kiri', lastname: 'Yakos' },  { firstname: 'Joran', lastname: 'Van der Sloot' },
+{ firstname: 'Jesse', lastname: 'van der Heijden' },
+]
