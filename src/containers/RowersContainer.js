edafef085de03ac5a37a9242df257665202827f6
VERANIKA <veranika.isakova89@gmail.com>
2018-01-03 10:12:28 +0100

    Add alphabet order to ships and rowers

--- src/containers/RowersContainer.js
+++ src/containers/RowersContainer.js
@@ -24,11 +24,18 @@ class RowersContainer extends PureComponent {
     <div>
       <div className = 'List'>
         <List>
-          {this.props.rowers.map((rower) => (
+          {this.props.rowers.sort(function(a, b){
+            if (a.firstname < b.firstname) {
+              return -1
+            } else if (a.firstname > b.firstname) {
+              return 1
+            } else {
+              return 0
+            }
+          }).map((rower) => (
             <ListItem key={rower.id} leftAvatar={
                <Avatar src= {avatar} size={30}  />}
                 style={{  margin: '50x', padding: '0,5rem',  }}
-
                 onClick={this.linkToOneRower(rower.id)}>{rower.firstname} {rower.lastname}
             </ListItem>
           ))}
