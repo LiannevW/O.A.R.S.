ef936899fe3e5e5da647f36e9361b448c2dc3c44
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2018-01-03 16:08:28 +0100

    started styling on shippage

--- src/App.js
+++ src/App.js
@@ -21,8 +21,10 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
-          <Navigation />
-          <Routes />
+          <div className="Content">
+            <Navigation />
+            <Routes />
+          </div>
           <Footer />
         </div>
       </MuiThemeProvider>
